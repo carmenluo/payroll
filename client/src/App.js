1b2eca62b3d2827a644ca6de1e936c9a4aa374dd
carmenluo <Sdluojm@gmail.com>
2019-11-12 20:34:28 -0500

    add bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,22 @@
 import React from 'react';
 import logo from './logo.svg';
+import papa from 'papaparse';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className='container'>
+        <div className='row'>
+          <div className='offset-md-3 col-md-6'>
+            <div className='form-group files'>
+              <label>Please select you file to upload</label>
+              <input type='file' accept=".csv" name='file' className='form-control' ></input>
+
+            </div>
+            <button type='button' className='btn btn-s' >Upload</button>
+          </div>
+        </div>
+      </div>
   );
 }
 
